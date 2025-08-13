c69e86f01e5500307a243599df2c7f5999b8c6c6
Riza Suminto <riza.suminto@cloudera.com>
2025-08-12 19:25:43 -0700

    Change to ImpylaHS2Connection
    Change-Id: I6fc11493631ae9d67bfdf624df3cd8dab6d4bf85

--- bin/load-data.py
+++ bin/load-data.py
@@ -33,7 +33,7 @@ import time
 import traceback
 
 from optparse import OptionParser
-from tests.common.impala_connection import create_connection
+from tests.common.impala_connection import ImpylaHS2Connection
 from tests.common.test_vector import HS2
 from multiprocessing.pool import ThreadPool
 
@@ -113,6 +113,8 @@ HIVE_ARGS = '-n %s -u "jdbc:hive2://%s/default;%s" --verbose=true'\
 
 HADOOP_CMD = os.path.join(os.environ['HADOOP_HOME'], 'bin/hadoop')
 
+HS2_HOST_PORT = "{}:{}".format(options.impalad, 21050)
+
 def available_workloads(workload_dir):
   return [subdir for subdir in os.listdir(workload_dir)
             if os.path.isdir(os.path.join(workload_dir, subdir))]
@@ -182,9 +184,8 @@ def exec_impala_query_from_file(file_name):
   LOG.info("Beginning execution of impala SQL on {0}: {1}".format(
            options.impalad, file_name))
   is_success = True
-  impala_client = create_connection(options.impalad + ":21050",
-                                    use_kerberos=options.use_kerberos,
-                                    protocol=HS2)
+  impala_client = ImpylaHS2Connection(HS2_HOST_PORT,
+                                      use_kerberos=options.use_kerberos)
   output_file = file_name + ".log"
   query = None
   with open(output_file, 'w') as out_file:
@@ -237,7 +238,7 @@ def generate_schema_statements(workload):
     generate_cmd += " --hive_warehouse_dir=%s" % options.hive_warehouse_dir
   if options.hdfs_namenode is not None:
     generate_cmd += " --hdfs_namenode=%s" % options.hdfs_namenode
-  generate_cmd += " --backend=%s:%d" % (options.impalad, 21050)
+  generate_cmd += " --backend=%s" % HS2_HOST_PORT
   LOG.info('Executing Generate Schema Command: ' + generate_cmd)
   schema_cmd = os.path.join(TESTDATA_BIN_DIR, generate_cmd)
   error_msg = 'Error generating schema statements for workload: ' + workload
